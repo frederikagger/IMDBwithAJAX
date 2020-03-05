9a6686e184ef52c7f2944aa519d57890a0b4e141
frederikagger <frederikagger@hotmail.com>
2020-03-05 18:02:13 +0100

    dropdown tilføjet

--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -1,26 +1,29 @@
 let txt;
 let titles;
+var list = document.getElementById("ajax");
 
-/*
-$(document).ready(function () {
-    $("#button").click(ajaxVanilla())
-}),
-    $("#input").keyup(getInputTxt());
-*/
 function ajaxVanilla(txt) {
     const xhr = new XMLHttpRequest();
+
     xhr.open("GET", "http://www.omdbapi.com/?apikey=cd534c5c&type=movie&s="+txt, true);
     xhr.onreadystatechange = function (){
-        if(this.readyState===4 && this.status==200) {
+        if(this.readyState===4 && this.status===200) {
             titles = "";
             var obj = JSON.parse(this.responseText);
             var movies = obj["Search"];
-           if (typeof movies !== (undefined || null)){
+           try{
                for (let i=0; i<Object.values(movies).length; i++){
-                   titles = titles + Object.values(movies)[i]["Title"]+" "+Object.values(movies)[i]["Year"]+" <br>";
+                   var option = document.createElement("option");
+                   option.value = Object.values(movies)[i]["Title"];
+                   if (option!==null|undefined){
+                       list.appendChild(option);
+                   }
+
+                   //titles = titles + Object.values(movies)[i]["Title"]+"<br>";
                }
+           } catch (e) {
+               console.log(e)
            }
-           document.getElementById("json").innerHTML = titles;
         }
     }
     xhr.send();
@@ -31,5 +34,4 @@ function getInputTxt() {
     if (txt.length>=2) {
         ajaxVanilla(txt);
     }
-
 }
